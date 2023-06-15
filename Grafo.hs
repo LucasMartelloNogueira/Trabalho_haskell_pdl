569035987e7d2dbda6a67ca6ecc7ccc3ae9c66f5
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2023-06-15 10:35:44 -0300

    add func de execução sequencial

--- Grafo.hs
+++ Grafo.hs
@@ -21,3 +21,33 @@ temAresta ((_, _, x):xs) aresta = x == aresta || temAresta xs aresta
 temArestaNoVerticeOrigem :: [(String, String, String)] -> String -> String -> Bool
 temArestaNoVerticeOrigem [] vertice aresta = False
 temArestaNoVerticeOrigem ((v, _, ar):xs) vertice aresta = ((v == vertice) && (ar == aresta)) || temArestaNoVerticeOrigem xs vertice aresta
+
+-- pega todas as arestas com determinado rotulo
+getArestasComRotulo :: [(String, String, String)] -> String -> [(String, String, String)]
+getArestasComRotulo [] _ = []
+getArestasComRotulo arestas rotulo = [(v1, v2, rot) | (v1, v2, rot) <- arestas, rot == rotulo]
+
+-- pega todas as arestas com detesrminado rotulo a partir de um vertice de origem
+-- arg 1: lista de arestas do grafo
+-- arg 2: rotulo testado
+-- arg 3: vertice testado
+getRotas :: [(String, String, String)] -> String -> String -> [(String, String, String)]
+getRotas [] _ _ = []
+getRotas arestas rotulo vertice = filter (\(v1, v2, rot) -> v1 == vertice && rot == rotulo) arestas
+
+
+
+-- funcao para verificar execucao sequecial de programas
+-- arg 1: arestas do grafo
+-- arg 2: vertice que esta sendo atualmente testado
+-- arg 3: lista de rotulos das arestas (progamas, ex: alfa)
+-- retorno: bool se todas as arestas existem no programa
+temExeSequencial :: [(String, String, String)] -> String -> [String] -> Bool
+temExeSequencial arestas vertice [rotulo] = or [v1 == vertice && rot == rotulo | (v1, v2, rot) <- getRotas arestas rotulo vertice]
+temExeSequencial arestas vertice (rotulo:rotulos) = 
+    let rotas = getRotas arestas rotulo vertice
+    in
+        if rotas == [] then False
+        else
+            or [temExeSequencial arestas v2 rotulos | (v1, v2, rotulo) <- rotas]
+
--- Main.hs
+++ Main.hs
@@ -4,6 +4,11 @@ import Grafo
 
 main ::IO()
 main = do
-    let g = Grafo {vertices = ["a", "b", "c"], arestas = [("a", "b", "alfa"), ("b", "c", "beta")]}
-    let res = temArestaNoVerticeOrigem (getArestas g) "b" "Gama"
-    print(res)
+    let g = Grafo {vertices = ["a", "b", "c", "d", "e"], arestas = [("a", "b", "alfa"), ("b", "c", "beta"), ("a", "d", "alfa"), ("c", "e", "gama")]}
+    let arestas = getArestas g
+
+    -- teste com execução sequencial (ex: vendo se tem no grafo a pdf(alfa;beta;gama))
+    let seq = ["alfa", "beta", "gama"]
+    let vertice = "a"
+    let teste = temExeSequencial arestas vertice seq
+    print(teste)
