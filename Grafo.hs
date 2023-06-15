66d6a1ddc59a0d00e5a3efcbd12c209ee2547c4b
Lucas Martello Nogueira <lucas.martello0509@gmail.com>
2023-06-15 19:35:39 -0300

    add funcoes de escolha nao deterministica

--- Grafo.hs
+++ Grafo.hs
@@ -36,13 +36,14 @@ getRotas [] _ _ = []
 getRotas arestas rotulo vertice = filter (\(v1, v2, rot) -> v1 == vertice && rot == rotulo) arestas
 
 
-
--- funcao para verificar execucao sequecial de programas
+-- funcao para verificar execucao sequecial de um programa pdl (ex: alfa;beta)
 -- arg 1: arestas do grafo
 -- arg 2: vertice que esta sendo atualmente testado
 -- arg 3: lista de rotulos das arestas (progamas, ex: alfa)
 -- retorno: bool se todas as arestas existem no programa
 temExeSequencial :: [(String, String, String)] -> String -> [String] -> Bool
+temExeSequencial [] _ _ = False -- TODO: testar se isso faz sentido (caso em que n tem arestas)
+temExeSequencial _ _ [] = True -- TODO: testar se isso faz sentido (caso em que n tem rotulos)
 temExeSequencial arestas vertice [rotulo] = or [v1 == vertice && rot == rotulo | (v1, v2, rot) <- getRotas arestas rotulo vertice]
 temExeSequencial arestas vertice (rotulo:rotulos) = 
     let rotas = getRotas arestas rotulo vertice
@@ -51,3 +52,32 @@ temExeSequencial arestas vertice (rotulo:rotulos) =
         else
             or [temExeSequencial arestas v2 rotulos | (v1, v2, rotulo) <- rotas]
 
+
+-- pega todas as arestas (com diferentes rotulos) a partir de um vértice
+-- agr 1: arestas do grafo
+-- arg 2: vertice testado
+getDifRotas :: [(String, String, String)] -> String -> [(String, String, String)]
+getDifRotas [] _ = []
+getDifRotas arestas vertice = [(v1, v2, rotulo) | (v1, v2, rotulo) <- arestas, v1 == vertice]
+
+
+-- funcao para verificar execucao de escolha não deterministica de pdl (ex: alfa U beta)
+-- arg 1: arestas do grafo
+-- arg 2: vertice que sera testado
+-- arg 3: rotulos que serão testados (as diferetes opções na escolha não deterministica)
+temEscolhaNaoDet :: [(String, String, String)] -> String -> [String] -> Bool
+temEscolhaNaoDet [] _ _ = False
+temEscolhaNaoDet arestas vertice rotulos = or [elem rot rotulos | (v1, v2, rot) <- getDifRotas arestas vertice]
+
+
+-- TODO: implementar funcao de loop
+-- funcao que verifica existencia de um loop no grafico
+-- arg 1: arestas do grafo
+-- arg 2: vertice testado
+-- arg 3: rotulo (programa) testado (ex: alfa)
+temIteracaoNaoDet :: [(String, String, String)] -> String -> String -> Bool
+temIteracaoNaoDet [] _ _ = True -- caso em que não executa nenhuma vez
+temIteracaoNaoDet arestas vertice rotulo =
+    let soRotasDiferentes = or [rot /= rotulo |(v1, v2, rot) <- getDifRotas arestas vertice]
+    in
+        soRotasDiferentes || or [temIteracaoNaoDet arestas v2 rotulo | (v1, v2, rot) <- getRotas arestas rotulo vertice]
--- Main.hs
+++ Main.hs
@@ -4,11 +4,35 @@ import Grafo
 
 main ::IO()
 main = do
-    let g = Grafo {vertices = ["a", "b", "c", "d", "e"], arestas = [("a", "b", "alfa"), ("b", "c", "beta"), ("a", "d", "alfa"), ("c", "e", "gama")]}
+    let g = Grafo {vertices = ["a", "b", "c", "d", "e"], arestas = [("a", "b", "alfa"), ("b", "c", "beta"), ("a", "d", "delta"), ("c", "e", "gama")]}
+
+    -- ilustracao grafo
+    --          a
+    --    alfa / \ delta
+    --        b    d
+    --  beta /     
+    --      c
+    --       \ gama
+    --        e
+
+    -- arestas do grafo
     let arestas = getArestas g
 
-    -- teste com execução sequencial (ex: vendo se tem no grafo a pdf(alfa;beta;gama))
+    -- teste com execução sequencial (ex: vendo se tem no grafo a pdl: (alfa;beta;gama) )
     let seq = ["alfa", "beta", "gama"]
     let vertice = "a"
     let teste = temExeSequencial arestas vertice seq
     print(teste)
+
+    --  teste com execução de escolha não deterministica (ex: vendo se tem no grafo a pdl: alfa U delta)
+    -- let possiveis = ["beta", "gama"]
+    -- let vertice = "a"
+    -- let teste = temEscolhaNaoDet arestas vertice possiveis
+    -- print(teste)
+
+    -- teste com execucao de iteracao nao deterministica (ex: (alfa)*)
+    -- let rotulo = "alfa"
+    -- let vertice = "a"
+    -- let teste = temIteracaoNaoDet arestas vertice rotulo
+    -- print(teste)
+
